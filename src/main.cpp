19fe86510efdfd8b3e9ac42df46873487f0ecc21
Byron Steven Vega Chavarría <151584914+ShyManifold@users.noreply.github.com>
2024-06-06 15:10:32 -0600

    Solution to ff0

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,22 @@
 #include <sstream>
 #include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  //std::cout << "Logs from your program will appear here!\n";
+  // std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  while (true)
+  {
+
+    std::cout << "$ ";
+    
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
