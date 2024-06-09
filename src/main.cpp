392fa1886936359a784fc1057fb0a6c9d1afa0b2
Byron Steven Vega Chavarría <151584914+ShyManifold@users.noreply.github.com>
2024-06-09 16:43:32 -0600

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,6 @@ int main(int argc, char *argv[])
   std::string pathArg = std::getenv("PATH");
   Application app(pathArg);
 
-  for (size_t i = 0; i < argc; i++)
-  {
-    std::cout << argv[i] << std::endl;
-  }
-
   app.m_run();
 
   return 0;
